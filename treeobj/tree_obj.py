d0ce77742e69afbe2a85dc928d4377fa2e912da4
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-11-24 17:36:09 +0300

    fix

--- treeobj/tree_obj.py
+++ treeobj/tree_obj.py
@@ -4,6 +4,9 @@
 import os
 
 # from tools_objects import obj_sha1
+# from blobobj.blob_obj import BlobObject
+
+from os.path import isfile, join
 
 
 class TreeObject:
@@ -13,12 +16,25 @@ class TreeObject:
             <права файла> <тип объекта(tree or blob)> <sha1 объекта> <имя файла>
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, input_dir="./", output_dir='.igit/objects/'):
+        self.input_dir = input_dir  # папка которую нужно обработать
+        self.oput_dir = output_dir  # папка в которую сохранять результат дерева в виде файла
+        self.treeobj = []  # все записи дерева
+
+    def addfiles(self):
+        """
+        добавление всех файлов в дерево
+        :return:
+        """
+        # найти все файлы в папке
+        files = [f for f in os.listdir(self.input_dir) if isfile(join(self.input_dir, f))]
+        for ff in files:
+            print(ff)
 
 
 def main():
-    pass
+    tobj = TreeObject()
+    tobj.addfiles()
 
 
 if __name__ == '__main__':
