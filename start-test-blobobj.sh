c98a2f05f988358f6a5aa02add4d848fa8432506
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-11-24 11:30:30 +0300

    добавлен один параметр который можно укзаать скрипту который запускает тест

--- start-test-blobobj.sh
+++ start-test-blobobj.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # запуск тестов для проверки Blob Oblects
 # ключ -s указывает путь где находятся
-python -m unittest discover -s blobobj/tests/
+python -m unittest discover -s blobobj/tests/ $1
