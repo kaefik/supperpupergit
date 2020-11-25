ecf3cd6126efee2f47651a73ff9040fda9079872
Ilnur Saifutdinov <ilnursoft@gmail.com>
2020-11-25 22:41:45 +0300

    добавлен тест и функциональность в TreeObject поиск всех файлов и папкок в указанном месте

--- treeobj/tests/test_treeobj.py
+++ treeobj/tests/test_treeobj.py
@@ -9,7 +9,26 @@ class BlobObjectTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.current_dir_test = os.path.dirname(os.path.abspath(__file__))
+        cls.current_dir_test = os.path.dirname(os.path.abspath(__file__)) + '/'
 
-    def test_empty(self):
-        pass
+    def test_get_all_files_and_directory_1(self):
+        """
+        проверка получения всех файлов и папок в папке  test-files/1/
+        """
+        treeObj = TreeObject(input_dir=self.current_dir_test + 'test-files/1/',
+                             output_dir=self.current_dir_test + 'test-files/out/')
+        treeObj.get_all_files_and_directory()
+
+        self.assertEqual(treeObj._files, {'file1.txt'})
+        self.assertEqual(treeObj._directory, set())
+
+    def test_get_all_files_and_directory_2(self):
+        """
+        проверка получения всех файлов и папок в папке  test-files/2/
+        """
+        treeObj = TreeObject(input_dir=self.current_dir_test + 'test-files/2/',
+                             output_dir=self.current_dir_test + 'test-files/out/')
+        treeObj.get_all_files_and_directory()
+
+        self.assertEqual(treeObj._files, {'1.py', 'file1.txt'})
+        self.assertEqual(treeObj._directory, {'papka1'})
--- treeobj/tree_obj.py
+++ treeobj/tree_obj.py
@@ -10,8 +10,8 @@ from os.path import isfile, join
 
 # тип объекта
 NOOBJ = 0  # нет объекта
-BLOB = 1   # объект Blob
-TREE = 2   # объект Tree
+BLOB = 1  # объект Blob
+TREE = 2  # объект Tree
 
 
 class ItemTreeObject:
@@ -26,9 +26,9 @@ class ItemTreeObject:
 
     def __init__(self, right_access='', typeobj=NOOBJ, sha1='', filename=''):
         self.right_access = right_access  # <права файла>
-        self.typeobj = typeobj            # <тип объекта(tree or blob)>
-        self.sha1 = sha1                  # <sha1 объекта>
-        self.filename = filename          # <имя файла>
+        self.typeobj = typeobj  # <тип объекта(tree or blob)>
+        self.sha1 = sha1  # <sha1 объекта>
+        self.filename = filename  # <имя файла>
 
 
 class TreeObject:
@@ -38,27 +38,30 @@ class TreeObject:
             <права файла> <тип объекта(tree or blob)> <sha1 объекта> <имя файла>
     """
 
-    parent_tree = None  # Tree родитель
-
-    def __init__(self, input_dir="./", output_dir='.igit/objects/'):
+    def __init__(self, input_dir='./', output_dir='.igit/objects/'):
         self.input_dir = input_dir  # папка которую нужно обработать
-        self.oput_dir = output_dir  # папка в которую сохранять результат дерева в виде файла
-        self.treeobj = []  # все записи дерева
+        self.output_dir = output_dir  # папка в которую сохранять результат дерева в виде файла
+        self.obj = set()  # все записи дерева
+        self.parent_tree = None  # Tree родитель
+        self._files = set()  # все найденные файлы в папке self.input_dir
+        self._directory = set()  # все найденные папки в папке self.input_dir
 
-    def findallfiles(self):
+    def get_all_files_and_directory(self):
         """
-        добавление всех файлов в дерево
+        поиск всех файлов и папок в self.input_dir
         :return:
         """
-        # найти все файлы в папке
-        files = [f for f in os.listdir(self.input_dir) if isfile(join(self.input_dir, f))]
-        for ff in files:
-            print(ff)
+        self._files = set()
+        self._directory = set()
+        for f in os.listdir(self.input_dir):
+            if isfile(join(self.input_dir, f)):
+                self._files.add(f)
+            else:
+                self._directory.add(f)
 
 
 def main():
-    tobj = TreeObject()
-    tobj.addfiles()
+    pass
 
 
 if __name__ == '__main__':
