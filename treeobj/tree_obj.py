64db616c2f2f8611111def345af9b010904b2c1e
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-11-25 17:57:10 +0300

    добавлены некоторые моменты по элементу дерева (не доделано)

--- treeobj/tree_obj.py
+++ treeobj/tree_obj.py
@@ -8,6 +8,28 @@ import os
 
 from os.path import isfile, join
 
+# тип объекта
+NOOBJ = 0  # нет объекта
+BLOB = 1   # объект Blob
+TREE = 2   # объект Tree
+
+
+class ItemTreeObject:
+    """
+        элемент объекта Tree (одна запись), в TreeObject таких записей может быть несколько.
+        содержит следующие данные:
+            * <права файла>
+            * <тип объекта(tree or blob)>
+            * <sha1 объекта>
+            * <имя файла>
+    """
+
+    def __init__(self, right_access='', typeobj=NOOBJ, sha1='', filename=''):
+        self.right_access = right_access  # <права файла>
+        self.typeobj = typeobj            # <тип объекта(tree or blob)>
+        self.sha1 = sha1                  # <sha1 объекта>
+        self.filename = filename          # <имя файла>
+
 
 class TreeObject:
     """
@@ -16,12 +38,14 @@ class TreeObject:
             <права файла> <тип объекта(tree or blob)> <sha1 объекта> <имя файла>
     """
 
+    parent_tree = None  # Tree родитель
+
     def __init__(self, input_dir="./", output_dir='.igit/objects/'):
         self.input_dir = input_dir  # папка которую нужно обработать
         self.oput_dir = output_dir  # папка в которую сохранять результат дерева в виде файла
         self.treeobj = []  # все записи дерева
 
-    def addfiles(self):
+    def findallfiles(self):
         """
         добавление всех файлов в дерево
         :return:
