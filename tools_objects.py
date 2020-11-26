d629fa62f046ba0666971d4de3c49756e223c7e7
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-11-26 16:15:05 +0300

    добавлена функция поиска всех файлов и папок в указанной папке

--- tools_objects.py
+++ tools_objects.py
@@ -3,6 +3,7 @@
 у каждого Объекта имя записывается как 16-ричное представление этого хеша. длина хэша равна 20 байт.
 Первые два символа имеет объект и создает подиректорию с этим именем.
 """
+import os
 import hashlib
 
 
@@ -18,9 +19,24 @@ def obj_sha1(bstr):
     return hex_dig
 
 
+def get_file_dirs(input_dir):
+    """
+    получить все файлы и папки в указанной папке input_dir
+    """
+    files = set()
+    directory = set()
+    for f in os.listdir(input_dir):
+        if os.path.isfile(os.path.join(input_dir, f)):
+            files.add(f)
+        else:
+            directory.add(f)
+    return files, directory
+
+
 def main():
-    hh = obj_sha1('Privet как дела?')
-    print(type(hh))
+    # f, d = get_file_dirs('/home/oilnur/prj/prj-py/supperpupergit')
+    # print(d)
+
 
 if __name__ == '__main__':
     main()
