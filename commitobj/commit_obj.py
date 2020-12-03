fcd5e1267e87b3c690038588923863216ca9ef37
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-12-03 17:31:40 +0300

    добавлены заготовки  некоторых методов коммита

--- commitobj/commit_obj.py
+++ commitobj/commit_obj.py
@@ -4,4 +4,35 @@
 
 
 class CommitObject:
+    """
+    одному коммиту соответствует только одно дерево, хранить также ссылку на родительский коммит (-ы)
+        (кроме первого коммита), на автора, сообщение коммита.
+    """
+
+    def __init__(self, treeobj=None, parrent_commint=None, author=None, commiter=None, message_commit=None):
+        self.treeobj = treeobj  # ссылка на TreeObject
+        self.parrent_commint = parrent_commint  # родительский коммит (-ы) в виде массива
+        self.author = author  # автор коммита
+        self.commiter = commiter  # коммитер
+        self.message_commit = message_commit  # текстовое сообщение коммита
+        self.sha1 = None
+        self.date = None
+
+    def generate(self):
+        """
+        генерация содержимого коммита
+        return: возвращает sha1 содержимого коммита
+        """
+        return self.sha1
+
+    def save(self, output_dir='./'):
+        """
+        сохранение коммита в папку output_dir
+        """
+        self.generate()
+        # TODO: здесь должно быть  сохранение коммита
+
+    def check_exist_commit(self, check_dir=""):
+        pass
+
     pass
